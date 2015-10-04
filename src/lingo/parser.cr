6a01f5741d4151b06b1e13c14cff7cdf09b2ddcd
Robert Mosolgo <rdmosolgo@gmail.com>
2015-10-04 16:50:27 -0700

    fix(Parser) use LazyRule to handle recursive rules

--- src/lingo/parser.cr
+++ src/lingo/parser.cr
@@ -1,6 +1,25 @@
 class Lingo::Parser
+  alias RuleGenerator = Proc(Lingo::Rule)
+  class LazyRule < Lingo::Rule
+    def initialize(&rule_generator : RuleGenerator)
+      @rule_generator = rule_generator
+    end
+
+    def parse?(raw_input)
+      inner_rule.parse?(raw_input)
+    end
+
+    def as(name)
+      inner_rule.as(name)
+    end
+
+    private def inner_rule
+      @inner_rule ||= @rule_generator.call
+    end
+  end
+
   def initialize
-    @rules = {} of Symbol => Lingo::Rule
+    @rules = {} of Symbol => LazyRule
   end
 
   macro str(match)
@@ -9,10 +28,7 @@ class Lingo::Parser
 
   macro rule(rule_name, &block)
     def {{rule_name.id}}
-      @rules[{{rule_name}}] ||= begin
-        rule = {{block.body}}
-        rule
-      end
+      @rules[{{rule_name}}] ||= LazyRule.new { ({{block.body}}) as Lingo::Rule }
     end
   end
 
